116d0b44c40845c0122e71a7a1ba0600702b67ca
Sai Sashreek D <saisashreekd@gmail.com>
2024-06-18 13:34:49 +0530

    Header Component

---
+++ src/Components/Header/HeaderPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderNav from "./HeaderNav";
+import HeaderContent from "./HeaderContent";
+
+function HeaderPage() {
+  return (
+    <>
+      <HeaderNav />
+      <HeaderContent />
+    </>
+  );
+}
+
+export default HeaderPage;

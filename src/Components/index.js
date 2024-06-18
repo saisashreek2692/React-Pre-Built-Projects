4293b6f6605259afb1283863c42134751d65ca77
Sai Sashreek D <saisashreekd@gmail.com>
2024-06-18 13:33:47 +0530

    Custom Components to Objects

---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+export { default as Header } from "./Header/HeaderPage";
+export { default as About } from "./Content/AboutSection";
+export { default as Features } from "./Content/FeaturesSection"
+export { default as Teams } from "./Content/TeamsSection";
+export { default as Trust } from "./Content/Trusties";
+export { default as Footer } from "./Footer/FooterSection";
